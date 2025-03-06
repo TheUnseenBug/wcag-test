de4b9d16dc594539431f78cd014114a43ef901e1
dennis granheimer <dennis.granheimer@live.se>
2025-03-06 12:01:08 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Menues from "./menu";
+
+function App() {
+  return (
+    <>
+      <header className="p-5 text-white bg-amber-500 ">
+        <nav className="flex flex-wrap items-center justify-center w-full gap-2 lg:gap-5">
+          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4 hover:text-red-500">
+            Home
+          </a>
+          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">Product</a>
+          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">About us</a>
+          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">Contact us</a>
+          <Menues />
+        </nav>
+     
+      </header>
+
+      <main>
+        <section>
+          <img src="https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <h1>Heading</h1>
+          <button className=" py-2.5 px-5 text-lg bg-amber-500 text-white focus:outline-dotted focus:outline-green-500 outline-offset-2 outline-4 rounded-lg  hover:text-slate-200">
+          Click me!  
+          </button>
+        </section>
+      </main>
+
+    </>
+  );
+}
+
+export default App;
