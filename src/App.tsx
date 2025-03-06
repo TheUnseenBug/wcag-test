64c5c9c29e3b68900306c9f43603a702f4ddb372
dennis granheimer <dennis.granheimer@live.se>
2025-03-06 12:04:37 +0100

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,50 @@ function App() {
     <>
       <header className="p-5 text-white bg-amber-500 ">
         <nav className="flex flex-wrap items-center justify-center w-full gap-2 lg:gap-5">
-          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4 hover:text-red-500">
+          <a
+            role="NavigationOption"
+            href=""
+            className="text-sm lg:p-2 md:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4 hover:text-red-500"
+          >
             Home
           </a>
-          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">Product</a>
-          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">About us</a>
-          <a href="" className="text-sm lg:p-2 lg:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4">Contact us</a>
+          <a
+            role="NavigationOption"
+            href=""
+            className="text-sm lg:p-2 md:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4"
+          >
+            Product
+          </a>
+          <a
+            role="NavigationOption"
+            href=""
+            className="text-sm lg:p-2 md:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4"
+          >
+            About us
+          </a>
+          <a
+            role="NavigationOption"
+            href=""
+            className="text-sm lg:p-2 md:text-xl focus:text-red-500 focus:outline-dotted focus:outline-green-500 outline-offset-4 outline-4"
+          >
+            Contact us
+          </a>
           <Menues />
         </nav>
-     
       </header>
 
       <main>
         <section>
-          <img src="https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <img
+            src="https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
           <h1>Heading</h1>
           <button className=" py-2.5 px-5 text-lg bg-amber-500 text-white focus:outline-dotted focus:outline-green-500 outline-offset-2 outline-4 rounded-lg  hover:text-slate-200">
-          Click me!  
+            Click me!
           </button>
         </section>
       </main>
-
     </>
   );
 }
--- src/menu.tsx
+++ src/menu.tsx
@@ -1,59 +1,66 @@
-import React from 'react'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import {
-    ArchiveBoxXMarkIcon,
-    ChevronDownIcon,
-    PencilIcon,
-    Square2StackIcon,
-    TrashIcon,
-  } from '@heroicons/react/16/solid'
+  ArchiveBoxXMarkIcon,
+  ChevronDownIcon,
+  PencilIcon,
+  Square2StackIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
 const Menues = () => {
-    return (
-        <div className="relative text-right ">
-          <Menu>
-            <MenuButton className="inline-flex items-center lg:gap-2 gap-1 rounded-md bg-gray-800 py-1 px-2 text-xs font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700  focus:outline-4 focus:outline-green-500 outline-offset-2 focus:outline-dotted">
-              Options
-              <ChevronDownIcon className="size-4 fill-white/60" />
-            </MenuButton>
-    
-            <MenuItems
-              transition
-              anchor="bottom end"
-              className="w-52 origin-top-right rounded-xl border border-white/5 bg-black/85 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
-            >
-              <MenuItem>
-                <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                  <PencilIcon className="size-4 fill-white/30" />
-                  Edit
-                  <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>
-                </button>
-              </MenuItem>
-              <MenuItem>
-                <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                  <Square2StackIcon className="size-4 fill-white/30" />
-                  Duplicate
-                  <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                </button>
-              </MenuItem>
-              <div className="h-px my-1 bg-white/5" />
-              <MenuItem>
-                <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                  <ArchiveBoxXMarkIcon className="size-4 fill-white/30" />
-                  Archive
-                  <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd>
-                </button>
-              </MenuItem>
-              <MenuItem>
-                <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                  <TrashIcon className="size-4 fill-white/30" />
-                  Delete
-                  <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                </button>
-              </MenuItem>
-            </MenuItems>
-          </Menu>
-        </div>
-      )
-}
+  return (
+    <div className="relative text-right ">
+      <Menu>
+        <MenuButton className="inline-flex items-center lg:gap-2 gap-1 rounded-md bg-gray-800 py-1 px-2 md:px-3 md:py-1.5 text-xs md:text-sm font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700  focus:outline-4 focus:outline-green-500 outline-offset-2 focus:outline-dotted">
+          Options
+          <ChevronDownIcon className="size-4 fill-white/60" />
+        </MenuButton>
 
-export default Menues
+        <MenuItems
+          transition
+          anchor="bottom end"
+          className="w-52 origin-top-right rounded-xl border border-white/5 bg-black/85 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          <MenuItem>
+            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <PencilIcon className="size-4 fill-white/30" />
+              Edit
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">
+                ⌘E
+              </kbd>
+            </button>
+          </MenuItem>
+          <MenuItem>
+            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <Square2StackIcon className="size-4 fill-white/30" />
+              Duplicate
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">
+                ⌘D
+              </kbd>
+            </button>
+          </MenuItem>
+          <div className="h-px my-1 bg-white/5" />
+          <MenuItem>
+            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <ArchiveBoxXMarkIcon className="size-4 fill-white/30" />
+              Archive
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">
+                ⌘A
+              </kbd>
+            </button>
+          </MenuItem>
+          <MenuItem>
+            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <TrashIcon className="size-4 fill-white/30" />
+              Delete
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">
+                ⌘D
+              </kbd>
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+    </div>
+  );
+};
+
+export default Menues;
